e9114a7c3e25b0829b87b7efcf73ae6a8368a9e5
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-23 16:59:10 -0700

    adding fontawesome icon and text to 404 page

--- src/NotFound.js
+++ src/NotFound.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBug } from '@fortawesome/free-solid-svg-icons'
 
 const NotFound = () => {
+  const bugIcon = <FontAwesomeIcon icon={faBug} size="lg" />
   return (
     <div>
-      <h1>404</h1>
-      <h5>Sorry, page not found</h5>
+        <h3 className="not-found-num">404</h3>
+        <span className="bug-icon">{bugIcon}</span>
+        <h5 className="not-found-text">Sorry, page not found</h5>
     </div>
   );
 }
