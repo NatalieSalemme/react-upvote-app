863c94ae8caf4c80b126985dfc38f212b5ab3347
Natalie Salemme <NatalieSalemme@gmail.com>
2018-10-22 23:12:09 -0700

    formatting text with flexbox

--- src/Story.js
+++ src/Story.js
@@ -6,19 +6,23 @@ class Story extends React.Component {
     const { title, description, url, votes, author, authorAvatar, storyImage } = this.props;
     return (
       <div className="story-container">
-        <a href={url} target="_blank">
-        <h3 className="title">{title}</h3>
-        </a>
-        <p className="description">{description}</p>
+        <div>
         <p className="votes">{votes}</p>
-        <p className="author-avatar">{authorAvatar}</p>
-        <p className="author">{author}</p>
         <img
           className="story-img"
           src={require(`${storyImage}`)}
           alt={title.split(' ').slice(0, 3).join(' ')}
           rel="noopener noreferrer"
         />
+      </div>
+      <div>
+        <a className="title-link" href={url} target="_blank">
+        <h2 className="title">{title}</h2>
+        </a>
+        <p className="description">{description}</p>
+        <p className="author-avatar">{authorAvatar}</p>
+        <p className="author">Posted by: {author}</p>
+      </div>
 
       </div>
     );
